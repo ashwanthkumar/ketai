f102c6cc35a3eb37333f463f143e79580c89566f
jduran9.uic <jduran9.uic@c53ed73f-3798-31a6-1195-f0ca7f546160>
2010-08-18 20:36:44 +0000

    Initial version of processing sketch running from eclipse on simulator/device

---
+++ ProcessingAndroidTest/src/com/ProcessingAndroidTest/ProcessingAndroidTest.java
@@ -0,0 +1,16 @@
+package com.ProcessingAndroidTest;
+
+import processing.core.PApplet;
+
+public class ProcessingAndroidTest extends PApplet{
+	
+		public void setup() {
+			ellipse(50, 50, 80, 80);
+		}
+
+		public void draw() {
+			background(0);
+			ellipse(50, 50, 80, 80);			
+		}
+
+}
