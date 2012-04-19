6532f7cb71b632630312a5dc7f93a8ab472dbcc5
jduran9.uic@gmail.com <jduran9.uic@gmail.com@c53ed73f-3798-31a6-1195-f0ca7f546160>
2012-04-19 16:39:55 +0000

    helper class to allow the parsing of byte arrays into OSC messages.

---
+++ ketai/src/ketai/net/kOSCMessage.java
@@ -0,0 +1,24 @@
+package ketai.net;
+/*
+ * 	This class is a utility class that exposes the parseMessage(byte[] data) 
+ * 		protected method from the OscMessage class.  This allows us to parse
+ * 		byte arrays into OscMessages.  This is useful over serial links
+ * 		where IP is forsaken. It is identical to OscMessage for all usage
+ * 		purposes except the constructor and exposing the isValid flag as
+ * 		a means to checking if the byte array yielded a valide OSC message.
+ */
+import oscP5.OscMessage;
+
+public class kOSCMessage extends OscMessage{
+
+	public kOSCMessage(byte[] _data) {
+		super("");
+		this.parseMessage(_data);
+	}
+	
+	public boolean isValid()
+	{
+		return isValid;
+	}
+	
+}
