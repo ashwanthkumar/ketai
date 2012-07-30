a395968493117d74f44e6028e06a8038164fbaee
jduran9.uic <jduran9.uic@c53ed73f-3798-31a6-1195-f0ca7f546160>
2012-07-30 19:06:54 +0000

    initial version of class to access the on-board vibrating motor

---
+++ ketai/src/ketai/ui/KetaiVibrate.java
@@ -0,0 +1,42 @@
+package ketai.ui;
+
+import android.content.Context;
+import android.os.Vibrator;
+import processing.core.PApplet;
+
+public class KetaiVibrate {
+	private PApplet parent;
+	private Vibrator vibe;
+	
+	public KetaiVibrate(PApplet _parent)
+	{
+		parent = _parent;
+		vibe = (Vibrator)parent.getSystemService(Context.VIBRATOR_SERVICE);
+	}
+
+	boolean hasVibrator(){
+		return vibe.hasVibrator();
+	}
+	
+	public void vibrate()
+	{
+		//forever! (well...almost)
+		long[] pattern = {0, Long.MAX_VALUE};
+		vibe.vibrate(pattern, Integer.MAX_VALUE);
+	}
+	
+	public void vibrate(long _duration)
+	{
+		vibe.vibrate(_duration);
+	}
+	
+	public void vibrate(long[] pattern, int repeat)
+	{
+		vibe.vibrate(pattern, repeat);
+	}
+	
+	public void stop()
+	{
+		vibe.cancel();
+	}
+}
