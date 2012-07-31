b9e3e8d1284d503df6dc3c7d8d187846aa6b325e
jduran9.uic <jduran9.uic@c53ed73f-3798-31a6-1195-f0ca7f546160>
2012-07-31 20:51:48 +0000

    added onRotate, onPinch support...

--- ketai/src/ketai/ui/KetaiGestures.java
+++ ketai/src/ketai/ui/KetaiGestures.java
@@ -1,8 +1,10 @@
 package ketai.ui;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 import processing.core.PApplet;
+import processing.core.PVector;
 import android.view.GestureDetector;
 import android.view.GestureDetector.OnDoubleTapListener;
 import android.view.GestureDetector.OnGestureListener;
@@ -14,11 +16,14 @@ public class KetaiGestures implements OnGestureListener, OnDoubleTapListener {
 	GestureDetector gestures;
 	KetaiGestures me;
 	Method onDoubleTapMethod, onScrollMethod, onFlingMethod, onTapMethod,
-			onLongPressMethod;
+			onLongPressMethod, onPinchMethod, onRotateMethod;
+	HashMap<Integer, PVector> cursors = new HashMap<Integer, PVector>();
+	HashMap<Integer, PVector> pcursors = new HashMap<Integer, PVector>();
 
 	public KetaiGestures(PApplet _parent) {
 		parent = _parent;
-		me = this;
+		me = this; // self reference for UI-thread constructor hackiness
+
 		parent.runOnUiThread(new Runnable() {
 			public void run() {
 				gestures = new GestureDetector(parent, me);
@@ -29,7 +34,6 @@ public class KetaiGestures implements OnGestureListener, OnDoubleTapListener {
 	}
 
 	public boolean onDown(MotionEvent arg0) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
@@ -37,8 +41,9 @@ public class KetaiGestures implements OnGestureListener, OnDoubleTapListener {
 			float arg3) {
 		if (onFlingMethod != null) {
 			try {
-				onFlingMethod.invoke(parent, new Object[] { arg0.getX(),
-						arg0.getY(), arg1.getX(), arg1.getY(), arg3 });
+				onFlingMethod.invoke(parent,
+						new Object[] { arg1.getX(), arg1.getY(), arg0.getX(),
+								arg0.getY(), arg3 });
 			} catch (Exception e) {
 			}
 		}
@@ -48,8 +53,8 @@ public class KetaiGestures implements OnGestureListener, OnDoubleTapListener {
 	public void onLongPress(MotionEvent arg0) {
 		if (onLongPressMethod != null) {
 			try {
-				onLongPressMethod.invoke(parent,
-						new Object[] { arg0.getX(), arg0.getY() });
+				onLongPressMethod.invoke(parent, new Object[] { arg0.getX(),
+						arg0.getY() });
 			} catch (Exception e) {
 			}
 		}
@@ -78,19 +83,41 @@ public class KetaiGestures implements OnGestureListener, OnDoubleTapListener {
 	}
 
 	public boolean surfaceTouchEvent(MotionEvent event) {
-		parent.onTouchEvent(event);
-		return gestures.onTouchEvent(event);
-	}
 
-	public boolean onDoubleTapEvent(MotionEvent arg0) {
-		if (onDoubleTapMethod != null) {
-			try {
-				onDoubleTapMethod.invoke(parent, new Object[] { arg0.getX(),
-						arg0.getY() });
-			} catch (Exception e) {
+		int code = event.getAction() & MotionEvent.ACTION_MASK;
+		int index = event.getAction() >> MotionEvent.ACTION_POINTER_ID_SHIFT;
+
+		float x = event.getX(index);
+		float y = event.getY(index);
+		int id = event.getPointerId(index);
+
+		if (code == MotionEvent.ACTION_DOWN
+				|| code == MotionEvent.ACTION_POINTER_DOWN) {
+			cursors.put(id, new PVector(x, y));
+		} else if (code == MotionEvent.ACTION_UP
+				|| code == MotionEvent.ACTION_POINTER_UP) {
+			if (cursors.containsKey(id))
+				cursors.remove(id);
+			if (pcursors.containsKey(id))
+				pcursors.remove(id);
+
+		} else if (code == MotionEvent.ACTION_MOVE) {
+			int numPointers = event.getPointerCount();
+			for (int i = 0; i < numPointers; i++) {
+				id = event.getPointerId(i);
+				x = event.getX(i);
+				y = event.getY(i);
+				if (cursors.containsKey(id))
+					pcursors.put(id, cursors.get(id));
+				else
+					pcursors.put(id, new PVector(x, y));
+
+				cursors.put(id, new PVector(x, y));
 			}
 		}
-		return true;
+		analyse();
+		parent.onTouchEvent(event);
+		return gestures.onTouchEvent(event);
 	}
 
 	public boolean onSingleTapConfirmed(MotionEvent arg0) {
@@ -99,8 +126,14 @@ public class KetaiGestures implements OnGestureListener, OnDoubleTapListener {
 	}
 
 	public boolean onDoubleTap(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		if (onDoubleTapMethod != null) {
+			try {
+				onDoubleTapMethod.invoke(parent, new Object[] { arg0.getX(),
+						arg0.getY() });
+			} catch (Exception e) {
+			}
+		}
+		return true;
 	}
 
 	private void findParentIntentions() {
@@ -118,8 +151,10 @@ public class KetaiGestures implements OnGestureListener, OnDoubleTapListener {
 		}
 
 		try {
-			onFlingMethod = parent.getClass().getMethod("onFling",
-					new Class[] { float.class, float.class, float.class, float.class, float.class });
+			onFlingMethod = parent.getClass().getMethod(
+					"onFling",
+					new Class[] { float.class, float.class, float.class,
+							float.class, float.class });
 		} catch (Exception e) {
 		}
 
@@ -135,6 +170,66 @@ public class KetaiGestures implements OnGestureListener, OnDoubleTapListener {
 		} catch (Exception e) {
 		}
 
+		try {
+			onPinchMethod = parent.getClass().getMethod("onPinch",
+					new Class[] { float.class, float.class, float.class });
+		} catch (Exception e) {
+		}
+
+		try {
+			onRotateMethod = parent.getClass().getMethod("onRotate",
+					new Class[] { float.class, float.class, float.class });
+		} catch (Exception e) {
+		}
 	}
 
+	private synchronized void analyse() {
+		if (cursors.size() > 1 && pcursors.size() > 1) {
+			PVector c1, c2, p1, p2;
+			c1 = cursors.get(0);
+			p1 = pcursors.get(0);
+
+			c2 = cursors.get(1);
+			p2 = pcursors.get(1);
+
+			// only use cursors 1/2 for our gestures...for now
+			if (c1 == null || c2 == null || p1 == null || p2 == null)
+				return;
+
+			float midx = (c1.x + c2.x) / 2;
+			float midy = (c1.y + c2.y) / 2;
+
+			float dp = PApplet.dist(p1.x, p1.y, p2.x, p2.y);
+			float dc = PApplet.dist(c1.x, c1.y, c2.x, c2.y);
+
+			float oldangle = PApplet.atan2(PVector.sub(p1, p2).y,
+					PVector.sub(p1, p2).x);
+			float newangle = PApplet.atan2(PVector.sub(c1, c2).y,
+					PVector.sub(c1, c2).x);
+
+			float delta = (newangle - oldangle);
+
+			if (onPinchMethod != null) {
+				try {
+					onPinchMethod.invoke(parent, new Object[] { midx, midy,
+							dc - dp });
+				} catch (Exception e) {
+				}
+			}
+
+			if (onRotateMethod != null) {
+				try {
+					onRotateMethod.invoke(parent, new Object[] { midx, midy,
+							delta });
+				} catch (Exception e) {
+				}
+			}
+
+		}
+	}
+
+	public boolean onDoubleTapEvent(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
