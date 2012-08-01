2f337b7e36e9870c2939061f16e26352a9583514
jduran9.uic <jduran9.uic@c53ed73f-3798-31a6-1195-f0ca7f546160>
2012-08-01 16:12:08 +0000

    updates for v7 release  - no more toggle for kb, instead show/hide methods for better control...

---
+++ ketai/src/ketai/ui/KetaiKeyboard.java
@@ -0,0 +1,24 @@
+package ketai.ui;
+
+import android.view.inputmethod.*;
+import android.app.Activity;
+import android.content.Context;
+
+public class KetaiKeyboard {
+		
+	static public void show(Activity parent)
+	{
+		InputMethodManager imm = (InputMethodManager)parent
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.showSoftInput(parent.getCurrentFocus(), 0);
+	}
+	
+	static public void hide(Activity parent)
+	{
+		InputMethodManager imm = (InputMethodManager)parent
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(parent.getCurrentFocus().getWindowToken(), 0);
+		
+	}
+	
+}
